c2acf64a010c25b75a4c761b5abec054b1a7aa1f
greatlaki <greatlaki911@gmail.com>
2021-09-05 17:55:23 +0300

    we made connections between the models and made changes to the database

--- bboard/models.py
+++ bboard/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Bb(models.Model):
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
@@ -10,3 +11,11 @@ class Bb(models.Model):
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявление'
         ordering = ['-published']
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Haзвaниe')
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
