c6eb087edc47bbde575446e22aeac43058c895f1
greatlaki <greatlaki911@gmail.com>
2021-09-05 16:40:10 +0300

    func index fixes

--- bboard/views.py
+++ bboard/views.py
@@ -5,7 +5,5 @@ from django.template import loader
 from .models import *
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'bboard/index.html', {'bbs': bbs})
