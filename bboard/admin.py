f52b52e2399fa8074353285be0edddeffba009fe
greatlaki <greatlaki911@gmail.com>
2021-09-05 17:40:28 +0300

    model editor

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+class BbAdmin(admin.ModelAdmin):
+  list_display = ('title', 'content', 'price', 'puЬlished')
+  list_display_links = ('title', 'content')
+  search_fields = ('title', 'content',)
 
-admin.site.register(Bb)
+admin.site.register(Bb, BbAdmin)
 
