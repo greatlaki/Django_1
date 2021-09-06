541217c6e2dd2aecd9ffdbcc030ad1e2303a0cf9
greatlaki <greatlaki911@gmail.com>
2021-09-06 13:24:59 +0300

    made controllers-classes: BbCreateView

--- bboard/urls.py
+++ bboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
 ]
